5ed780561d538cc3444884c9d61d47fe3f319683
dhagandev <dhagan@utexas.edu>
2020-08-06 00:25:43 -0500

    Move help text to constants

--- commands/constants.js
+++ commands/constants.js
@@ -2,6 +2,20 @@ const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+const commands = [
+  "Hello! I heard you need some help. (:",
+  "You can get my attention by saying \"Isme, \" or \"-isme\" \n",
+  "\"I need your help\": I\'ll send you a DM with a list of possible commands.",
+  "\"help\": I\'ll send you a DM with a list of possible commands.",
+  "\"ask me about my character\": I'll ask a question for you about your character! Tell me all about them.",
+  "\"askme\": I'll ask a question for you about your character! Tell me all about them.",
+  "\"ask my character a question\": You can add \": (your character\'s name)\" after this prompt. I'll ask a question for your character to answer! Let them speak through you.",
+  "\"askcharacter\": You can add \": (your character\'s name)\" after this prompt. I'll ask a question for your character to answer! Let them speak through you.",
+  "\"give me a writing prompt\": I'll give you a prompt to write a short story about. I would love to see what you come up with!",
+  "\"prompt\": I'll give you a prompt to write a short story about. I would love to see what you come up with!"
+]
+
+
 const prompts = [
   `You look at a falling star and make a wish: "I wish for a million more wishes" You say smugly, a milion more 'falling stars' appear in the night sky and suddenly you realize you've just inadvertedly caused the apocalypse.`,
   `Years ago you made a deal with Death, that he would kill anyone you wished so long as you offered a single life to him in return. Death thought it would teach you the value of life, but he didn't count on you owning an ant farm.`,
@@ -29,7 +43,8 @@ const question = [
   `What’s your character’s type or archetype?`
 ]
 
-exports.getRandomInt = getRandomInt
+exports.getRandomInt = getRandomInt;
+exports.commands = commands;
 exports.prompts = prompts;
 exports.characterQuestions = characterQuestion;
 exports.questions = question;
--- commands/help_fancy.js
+++ commands/help_fancy.js
@@ -4,18 +4,8 @@ module.exports = {
   execute(msg, args) { dmHelp(msg) },
 };
 
-const commands = [
-  "Hello! I heard you need some help. (:",
-  "You can get my attention by saying \"Isme, \" or \"-isme\" \n",
-  "\"I need your help\": I\'ll send you a DM with a list of possible commands.",
-  "\"help\": I\'ll send you a DM with a list of possible commands.",
-  "\"ask me about my character\": I'll ask a question for you about your character! Tell me all about them.",
-  "\"askme\": I'll ask a question for you about your character! Tell me all about them.",
-  "\"ask my character a question\": You can add \": (your character\'s name)\" after this prompt. I'll ask a question for your character to answer! Let them speak through you.",
-  "\"askcharacter\": You can add \": (your character\'s name)\" after this prompt. I'll ask a question for your character to answer! Let them speak through you.",
-  "\"give me a writing prompt\": I'll give you a prompt to write a short story about. I would love to see what you come up with!",
-  "\"prompt\": I'll give you a prompt to write a short story about. I would love to see what you come up with!"
-]
+const constants = require('./constants')
+let commands = constants.commands
 
 function dmHelp(msg) {
   let dmMessage = ''
